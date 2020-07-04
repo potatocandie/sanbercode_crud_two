1e908388311bd76578cc363a1454b48ae9fc670f
Maftukhatul Arifin <maftukhatularifin13@gmail.com>
2020-07-04 14:42:48 +0700

    add controller

---
+++ resources/views/questions/index.blade.php
@@ -0,0 +1,5 @@
+@extends('main')
+
+@section('content')
+ini konten
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('/pertanyaan', 'PertanyaanController');
-Route::resource('/jawaban', 'JawabanController');
+Route::get('/pertanyaan', 'QuestionController@index');
